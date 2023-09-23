e33ff2b9539138078b396d5ab9e796d4629c9e76
Ali Can Yücel (The Coderman) <32604562+alicanyucel@users.noreply.github.com>
2023-09-23 15:00:00 +0300

    Update InDependency.cs

--- Loose_Coupling/InDependency.cs
+++ Loose_Coupling/InDependency.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Loose_Coupling
+namespace Loose_Coupling_InDependency;
+class MailSender
 {
-    internal class InDependency
+    public void Send(IMailServer mailServer)
     {
+        mailServer.Send("filanca@falanca.com", "LayyHn");
     }
 }
+interface IMailServer
+{
+    void Send(string to, string body);
+}
+class Gmail : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        //...
+    }
+}
+class Hotmail : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        //...
+    }
+}
+class Yandex : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        //...
+    }
