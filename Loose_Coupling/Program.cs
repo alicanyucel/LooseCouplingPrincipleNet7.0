376a43397dc8be77c1842b34e849856b151ee597
Ali Can Yücel (The Coderman) <32604562+alicanyucel@users.noreply.github.com>
2023-09-23 14:57:59 +0300

    Update Program.cs

--- Loose_Coupling/Program.cs
+++ Loose_Coupling/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Loose_Coupling_InDependency.MailSender sender = new();
+sender.Send(new Loose_Coupling_InDependency.Gmail());
+sender.Send(new Loose_Coupling_InDependency.Hotmail());
+
+//-------
+
+Loose_Coupling_Dependency.MailSender sender2 = new();
+sender2.Send();
+sender2.Send();
